357fe8105494605125a450e0c329dbe8d3b55697
bastien girschig <bastien.girschig@gmail.com>
2019-09-04 19:07:08 +0200

    single startup script

---
+++ start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script should be called from launchd/lingonX. It wont work directly
+
+# move to this script's directory
+cd "$(dirname "$0")"
+
+# add separator in the log files
+echo echo "----- $(date +'%m/%d/%Y %r')" >> logs-main.txt
+echo echo "----- $(date +'%m/%d/%Y %r')" >> log-front.txt
+echo echo "----- $(date +'%m/%d/%Y %r')" >> log-server.txt
+
+# Pipe this script's logs to a log file
+exec >> logs-main.txt 2>&1
+
+# Trap siging and err, so that we can clean processes
+trap clean_processes SIGINT
+trap clean_processes ERR
+
+# cleanup: kill all services (so that ports are free)
+function clean_processes() {
+  echo '_field cleanup';
+  jobs
+  kill %1;
+}
+
+# start detector server
+cd server/
+source env/bin/activate
+python server.py >> ../log-server.txt 2>&1 &
+
+# launch chrome
+osascript -e 'quit app "Google Chrome"'
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --start-fullscreen http://localhost:8080
+
+# Static serve files
+cd ../dist/
+serve -p 8080 >> ../log-front.txt 2>&1
