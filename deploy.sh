d8ffef278a9e98efcc2d0c2c4d694152434e0b0b
bastien girschig <bastien.girschig@gmail.com>
2019-09-05 13:45:39 +0200

    Deploy and start scripts (tested)

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,37 @@
-# TARGET=80.236.31.52 # barbes network
-TARGET=192.168.1.218 # la cense network
+# TARGET_HOST=bgirschig@80.236.31.52 # barbes network
+# TARGET_HOST=bgirschig@192.168.1.218 # la cense network
+
+TARGET_HOST=bgirschig@192.168.0.104 # musée de l'elysée
+TARGET_DIR=/Users/Shared/field
+
+ssh $TARGET_HOST <<- EOF
+mkdir -p $TARGET_DIR
+EOF
+
+scp ./start.sh $TARGET_HOST:$TARGET_DIR/
 
 # Build and deploy frontend
 yarn run build
-rsync -auvh ./dist/* bgirschig@$TARGET:~/Documents/frontend
+rsync -rltDuvh ./dist/* $TARGET_HOST:$TARGET_DIR/frontend
 
+# deploy detector service
 cd ./server
 source ./env/bin/activate
 /usr/local/bin/pip freeze > requirements.txt
-rsync -auvh ./* --exclude env/ --exclude recordings/ bgirschig@$TARGET:~/Documents/backend
+rsync -rltDuvh ./* --exclude env/ --exclude recordings/ $TARGET_HOST:$TARGET_DIR/server
 deactivate
 
-ssh bgirschig@$TARGET <<- EOF
-  cd ~/Documents/backend
+ssh $TARGET_HOST <<- EOF
+  cd $TARGET_DIR/server
   if [ ! -d env ]; then
     /Library/Frameworks/Python.framework/Versions/2.7/bin/virtualenv env
   fi
   
+  # This does not work, because the $PATH is the deployer computer's PATH. This
+  # is possibly because this is not an interactive shell (?)
+  # 
   # source env/bin/activate
   # pip install -r requirements.txt
 
   echo $(date +'%m/%d/%Y %r') ' -  update' >> ~/Desktop/log.txt
-  say ding
 EOF
--- start.sh
+++ start.sh
@@ -5,10 +5,13 @@
 # move to this script's directory
 cd "$(dirname "$0")"
 
+# Make sure we have all the tools we need
+PATH="/Users/Shared/commands/:$PATH"
+
 # add separator in the log files
-echo echo "----- $(date +'%m/%d/%Y %r')" >> logs-main.txt
-echo echo "----- $(date +'%m/%d/%Y %r')" >> log-front.txt
-echo echo "----- $(date +'%m/%d/%Y %r')" >> log-server.txt
+echo "----- $(date +'%m/%d/%Y %r')" >> logs-main.txt
+echo "----- $(date +'%m/%d/%Y %r')" >> log-front.txt
+echo "----- $(date +'%m/%d/%Y %r')" >> log-server.txt
 
 # Pipe this script's logs to a log file
 exec >> logs-main.txt 2>&1
@@ -22,6 +25,7 @@ function clean_processes() {
   echo '_field cleanup';
   jobs
   kill %1;
+  kill %2;
 }
 
 # start detector server
@@ -29,10 +33,10 @@ cd server/
 source env/bin/activate
 python server.py >> ../log-server.txt 2>&1 &
 
+# Static serve files
+cd ../frontend/
+serve -p 8080 >> ../log-front.txt 2>&1 &
+
 # launch chrome
 osascript -e 'quit app "Google Chrome"'
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --start-fullscreen http://localhost:8080
-
-# Static serve files
-cd ../dist/
-serve -p 8080 >> ../log-front.txt 2>&1
